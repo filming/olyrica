42fec23f13a6eabe55e3824137121f4471a71ef2
Filming <79931565+filming@users.noreply.github.com>
2024-08-30 09:56:53 -0400

    feat: Get a random lyric from local storage

--- src/olyrica/__init__.py
+++ src/olyrica/__init__.py
@@ -3,6 +3,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import zipfile
 from os.path import basename
+import random
 
 from ..xify import Xify
 
@@ -66,3 +67,57 @@ class Olyrica:
         logger.addHandler(handler)
 
         return logger
+
+    def get_valid_lyric(self):
+        """Get a lyric from storage and analyze it using AI."""
+
+        lyric = ""
+        is_valid_lyric = False
+
+        self.logger.info("Attempting to retrieve and validate a lyric.")
+
+        while not is_valid_lyric:
+            lyric = self.get_random_lyric()
+
+            is_valid_lyric = (
+                True  # we'll do some checking later to properly validate this
+            )
+
+        return lyric
+
+    def get_random_lyric(self):
+        """Get a random lyric from storage."""
+
+        # Get a random album
+        LYRICS_DIR_PATH = os.path.join("storage", "lyrics")
+        random_album = random.choice(os.listdir(LYRICS_DIR_PATH))
+        album_dir_path = os.path.join(LYRICS_DIR_PATH, random_album)
+
+        # Get a random song from the album
+        random_song = random.choice(os.listdir(album_dir_path))
+        song_file_path = os.path.join(album_dir_path, random_song)
+
+        # Get a random lyric from the song
+        song_title = None
+        song_lyrics = set()
+
+        with open(song_file_path, encoding="utf-8") as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line:
+                    if not song_title:
+                        song_title = line
+                    else:
+                        song_lyrics.add(line)
+
+        song_lyrics = list(song_lyrics)
+        random_song_lyric = random.choice(song_lyrics)
+
+        self.logger.info(
+            """Chosen lyric: " %s ". Chosen song: " %s ". Chosen album: " %s ".""",
+            random_song_lyric,
+            song_title,
+            random_album,
+        )
+
+        return random_song_lyric
